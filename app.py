ddbd9e74fc5f9b1710866d72f3afa16c863f8358
ESTUDIANTE <emmanuelagudelo1604@gmail.com>
2023-08-16 13:26:35 -0500

    (CREATE):routes,create,store,configuration db, conection db

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import imp
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
 app= Flask(__name__)
@@ -14,10 +13,28 @@ mysql.init_app(app)
 
 @app.route('/')
 def index():
+    return render_template('empleados/index.html')
 
-    sql = ""
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _name=request.form['txt_name']
+    _last_name=request.form['txt_last_name']
+    _email=request.form['txt_email']
+    _photo=request.files['txt_photo']
+
+    sql = "INSERT INTO `empleados` (`id`, `name`, `last_name`, `email`, `photo`) VALUES (NULL, %s, %s, %s, %s);"
+    
+    datos=(_name,_last_name,_email,_photo.filename)
+
     conn= mysql.connect()
-    curso=
+    cursor=conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
 
     return render_template('empleados/index.html')
 
